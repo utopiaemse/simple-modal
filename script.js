9a4105a43aee082594aa0f5d4c2968fb2896e991
utopiaemse <utopiaemse@gmail.com>
2024-08-14 23:54:20 +0330

    0.1

--- script.js
+++ script.js
@@ -4,24 +4,19 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-// console.log();
-// btnsOpenModal[1].style.backgroundColor = '#ffff22';
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
-    console.log('clicked');
-    console.log(this.textContent);
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
 }
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
